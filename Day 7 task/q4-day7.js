887f8b90ec6f0f5c025e0a077df02ef11d6498a8
aakj2010 <107898533+aakj2010@users.noreply.github.com>
2022-07-14 10:29:44 +0530

    Add files via upload

---
+++ Day 7 task/q4-day7.js
@@ -0,0 +1,21 @@
+class Uber
+{
+    constructor(basefare,costpermile,costperminute,timeofride,ridedistance,bookingfee){
+        {
+            this.basefare=basefare;
+            this.costpermile=costpermile;
+            this.costperminute=costperminute;
+            this.timeofride=timeofride;
+            this.ridedistance=ridedistance;
+            this.bookingfee=bookingfee;
+        }
+    }
+
+getfare()
+{
+    console.log(`The fare is : ${this.basefare+((this.costperminute*this.timeofride)+(this.costpermile*this.ridedistance))+this.bookingfee}`);
+}
+}
+
+let ride= new Uber(40,15,1.5,10,20,30);
+ride.getfare();
